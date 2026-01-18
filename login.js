a22b52a6dfa4ba468d593fb5748b133cf3453968
joao50-web <joao_marcos50@outlook.com>
2026-01-18 17:41:45 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,20 +1,11 @@
 function login() {
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
-  const erro = document.getElementById("erro");
 
-  erro.textContent = "";
-
-  if (!email || !senha) {
-    erro.textContent = "Preencha todos os campos.";
-    return;
-  }
-
-  // Login simulado
   if (email === "joao123" && senha === "123") {
-    localStorage.setItem("usuarioLogado", email);
+    localStorage.setItem("usuarioLogado", "true");
     window.location.href = "grade.html";
   } else {
-    erro.textContent = "Usuário ou senha inválidos.";
+    document.getElementById("erro").innerText = "Usuário ou senha inválidos";
   }
 }
